b93b14809a5d5bb4709f093e47d7ce01b814ce66
Ivan Stepaniuk <ivan@thecloakroom.nl>
2014-11-27 15:05:17 +0100

    adding timestamps when not present and changing datetimes into iso formatted strings

--- messagebus/__init__.py
+++ messagebus/__init__.py
@@ -3,6 +3,7 @@ import json
 import os
 import socket
 import inspect
+import datetime
 try:
     from consumer import Consumer
 except ImportError:
@@ -17,11 +18,21 @@ class MessageBus:
         self.consumer = Consumer(self.broker_url, queue_prefix)
 
     def publish(self, message, payload={}):
-        body = json.dumps(payload, ensure_ascii=False)
+        body = json.dumps(self._prepare_payload(payload), ensure_ascii=False)
         connection = pika.BlockingConnection(pika.URLParameters(self.broker_url))
         channel = connection.channel()
         channel.basic_publish(exchange=self.RABBITMQ_DEFAULT_EXCHANGE, routing_key=message, body=body)
         connection.close()
+
+    def _prepare_payload(self, payload):
+        def serialize(value):
+            if isinstance(value, datetime.datetime):
+                return value.isoformat()
+            return value
+        proc_payload = { k: serialize(v) for k, v in payload.items() }
+        if not proc_payload.has_key('timestamp'):
+            proc_payload['timestamp'] = datetime.datetime.utcnow().isoformat()
+        return proc_payload
 
     def subscribe(self, message, callback):
         self.consumer.subscribe(message, callback)
