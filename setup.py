5cccb85fadf4714fd216a474ead5af4179f48e9c
Iván Stepaniuk <istepaniuk@gmail.com>
2018-08-18 19:46:07 +0200

    updated setup.py in preparation for PyPI

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@ from setuptools import setup
 
 setup(
     name='messagebus',
-    version="1.0.5",
+    version='1.1.0',
     author='Ivan Stepaniuk',
-    author_email='istepaniuk+mb@gmail.com',
+    author_email='istepaniuk@gmail.com',
     description='Wrapper arround Pika to publish and subscribe domain events',
-    url='https://github.com/istepaniuk/messagebus',
-    packages=['messagebus'],
-    install_requires=open('requirements.txt', 'r').readlines(),
-    include_package_data=True,
     long_description=open('README.md').read(),
+    url='https://github.com/istepaniuk/messagebus',
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'
+    ],
+    license='MIT/X11',
+    packages=['messagebus'],
+    install_requires=['pika>=0.12', 'pyrabbit'],
+    test_require=['mamba>=0.8', 'expect'],
+    include_package_data=True,
 )
